ab518efe6fa2c2325484d1f78e5593e8a70f9341
michelanjeho <michelanjeho@gmail.com>
2022-06-20 22:35:10 +0900

    Add static synchronizedSet

--- src/main/java/com/example/springwebsocketclients/handler/implement/AlphaWebSocketHandler.java
+++ src/main/java/com/example/springwebsocketclients/handler/implement/AlphaWebSocketHandler.java
@@ -8,14 +8,22 @@ import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import javax.websocket.Session;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class AlphaWebSocketHandler implements ClientWebSocketHandler {
 
+    private static final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("Alpha afterConnectionEstablished, session id : {}, session : {}", session.getId(), session.getUri());
+        sessions.add(session);
     }
 
     @Override
@@ -31,6 +39,7 @@ public class AlphaWebSocketHandler implements ClientWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
         log.info("Alpha afterConnectionClosed, session id : {}, status : {}", session.getId(), closeStatus);
+        sessions.remove(session);
     }
 
     @Override
--- src/main/java/com/example/springwebsocketclients/handler/implement/BetaWebSocketHandler.java
+++ src/main/java/com/example/springwebsocketclients/handler/implement/BetaWebSocketHandler.java
@@ -8,14 +8,21 @@ import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class BetaWebSocketHandler implements ClientWebSocketHandler {
 
+    private static final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("Beta afterConnectionEstablished, session id : {}, session : {}", session.getId(), session.getUri());
+        sessions.add(session);
     }
 
     @Override
@@ -31,6 +38,7 @@ public class BetaWebSocketHandler implements ClientWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
         log.info("Beta afterConnectionClosed, session id : {}, status : {}", session.getId(), closeStatus);
+        sessions.remove(session);
     }
 
     @Override
